407c97e1038ed8699d644c4befb5f6044f18bea3
Akber Akhmedzadeh <akber_akhmedzadeh@mail.ru>
2020-04-01 14:55:00 +0400

    corona

---
+++ assets/js/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+
+
+	let xhr=new XMLHttpRequest()
+
+
+
+	xhr.onload=()=>{
+
+
+		let response=xhr.responseText;
+
+
+		let changeJson=JSON.parse(response);
+
+
+
+		for(val in changeJson)
+		{
+			
+
+			for(keys in changeJson[val])
+			{
+
+				$('tbody').append(`<tr><td>${changeJson[val][keys].Country}</td><td>${changeJson[val][keys].NewConfirmed}</td><td>${changeJson[val][keys].TotalConfirmed}</td><td>${changeJson[val][keys].TotalDeaths}</td></tr>`)
+			}
+
+		}	
+
+
+	}
+
+
+
+	xhr.open('GET','https://api.covid19api.com/summary',true);
+
+
+	xhr.send();
+
+
+
+	$("#search").on("keyup", function() {
+		let value = $(this).val().toLowerCase();
+		$("#mytables tr").filter(function() {
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
+
+
+
+
+})
+
+
